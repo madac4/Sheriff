9290a122c6b46bf9002ebe106977bf84a19bee17
Oebu Petru <anrei.bors@gmail.com>
2021-10-25 21:34:49 +0300

    phone slider

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,5 @@
+@@include('swiper.min.js');
+@@include('slider.js');
 const header = document.querySelector('header');
 if (header) {
     document.addEventListener('scroll', () => {
@@ -119,3 +121,108 @@ if (roadmapAccordionItem[0]) {
         })
     })
 }
+
+// external js: isotope.pkgd.js
+
+// init Isotope
+// var iso = new Isotope('.insta-section__grid', {
+//     itemSelector: '.el',
+//     layoutMode: 'fitRows'
+// });
+
+// var filterFns = {
+//     numberGreaterThan50: function (itemElem) {
+//         var number = itemElem.querySelector('.number').textContent;
+//         return parseInt(number, 10) > 50;
+//     },
+//     ium: function (itemElem) {
+//         var name = itemElem.querySelector('.name').textContent;
+//         return name.match(/ium$/);
+//     }
+// };
+
+// var filtersElem = document.querySelector('.insta-section__buttons');
+// filtersElem.addEventListener('click', function (event) {
+//     if (!matchesSelector(event.target, 'button')) {
+//         return;
+//     }
+//     var filterValue = event.target.getAttribute('data-filter');
+//     filterValue = filterFns[filterValue] || filterValue;
+//     iso.arrange({ filter: filterValue });
+// });
+
+// var buttonGroups = document.querySelectorAll('.insta-section__buttons');
+// for (var i = 0, len = buttonGroups.length; i < len; i++) {
+//     var buttonGroup = buttonGroups[i];
+//     radioButtonGroup(buttonGroup);
+// }
+
+// function radioButtonGroup(buttonGroup) {
+//     buttonGroup.addEventListener('click', function (event) {
+//         if (!matchesSelector(event.target, 'button')) {
+//             return;
+//         }
+//         buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+//         event.target.classList.add('is-checked');
+//     });
+// }
+
+
+var swiperMobile = new Swiper('.slider-phone__body', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    slideToClickedSlide: true,
+    centeredSlides: true,
+    loop: true,
+    //   autoplay: {
+    //     delay: 100000,
+    //   },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return `
+            <div class="${className}">
+                <div class="circle"></div>
+                <div class="line"></div>
+            </div>`
+        },
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+
+    breakpoints: {
+
+        // 640: {
+        //     freemode: true,
+        //     slidesPerView: 3,
+        //     spaceBetween: 20,
+        // },
+        // 320: {
+        //     freemode: true,
+        //     slidesPerView: 3,
+        //     spaceBetween: 20,
+        // }
+    }
+
+});
+
+const pagination = document.querySelector('.slider-phone-controls__dots .swiper-pagination');
+const sliderBullets = [...pagination.getElementsByClassName("swiper-pagination-bullet")];
+
+sliderBullets.map((bullet) => {
+    bullet.onclick = () => {
+        i = sliderBullets.indexOf(bullet);
+
+        setTimeout(() => {
+            if (bullet.classList.contains !== 'swiper-pagination-bullet-active') {
+                for (i; i >= 0; --i) sliderBullets[i].classList.add('swiper-pagination-bullet-active');
+            } else {
+                for (i; i < sliderBullets.length; ++i) sliderBullets[i].classList.remove('swiper-pagination-bullet-active');
+            }
+        }, 5);
+    };
+});
